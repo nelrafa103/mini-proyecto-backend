266a2f90f30d56d225704c9a3e411a61aa000e61
Nelcido Diaz <t6iyhoqc9@mozmail.com>
2022-03-23 22:20:04 -0400

    Adding the capable of insert

--- db/connnection.go
+++ db/connnection.go
@@ -2,9 +2,12 @@ package db
 import (
     "github.com/go-pg/pg/v10"
 "os"
+"fmt"
 "log"
 )
 func ConnectToDb() {
+  os.Setenv("DB_HOST",)
+ //fmt.Println(os.Getenv("DB_HOST"),"ec2-3-222-204-187.compute-1.amazonaws.com")
 db:= pg.Connect(&pg.Options{
   Addr: os.Getenv("DB_HOST"),
   User: os.Getenv("DB_USER"),
